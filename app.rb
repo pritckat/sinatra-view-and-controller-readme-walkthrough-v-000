8dd749eedcd9d01464d1d22a29ef206d5a654003
Katrina Pritchard <pritckat@gmail.com>
2018-09-10 20:06:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
     erb :friends
 
   end
